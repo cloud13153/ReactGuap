543c91fd194290d5aa788717c23d2d22ab22d251
cloud13153 <cloudfirecxx11@mail.ru>
2024-11-09 21:40:00 +0500

    1

--- src/components/Circle/index.tsx
+++ src/components/Circle/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-
 import { Circle } from "../../types";
 
+// Интерфейс для пропсов компонента
 interface ICircle {
-    circle: Circle;
-    isSelected: boolean;
-    handleMouseDown: (
+    circle: Circle;          // Объект круга с его параметрами
+    isSelected: boolean;     // Флаг выбранного состояния
+    handleMouseDown: (      // Обработчик нажатия мыши
       e: React.MouseEvent<HTMLElement>,
       circle: Circle
     ) => void;
@@ -16,9 +16,13 @@ const Circle: React.FC<ICircle> = ({
     isSelected,
     handleMouseDown
 }) => {
+    // Определяем цвет границы в зависимости от цвета круга
+    // Если круг темный - граница белая, если светлый - черная
     const borderColor = parseInt(circle.color.substring(1), 16) < 0x808080 ?
       '#ffffff' :
       '#000000';
+
+    // Базовые стили с условным добавлением тени для выбранного состояния
     const styles = `
         absolute rounded-full cursor-mov
         ${isSelected &&'shadow-md' }
@@ -29,12 +33,12 @@ const Circle: React.FC<ICircle> = ({
         key={circle.id}
         className={styles}
         style={{
-          width: circle.radius * 2,
+          width: circle.radius * 2,    // Диаметр круга
           height: circle.radius * 2,
-          left: circle.x,
+          left: circle.x,              // Позиционирование
           top: circle.y,
           backgroundColor: circle.color,
-          ...isSelected && {
+          ...isSelected && {           // Добавляем границу если круг выбран
             border: borderColor
           }
         }}
